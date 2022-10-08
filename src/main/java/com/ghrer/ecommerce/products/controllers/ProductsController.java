80185cc933f8ba3f4ce6caf57b3f4c1efa4d76c1
Hussein Ghrer <hussin.ghrer@gmail.com>
2022-10-08 11:14:36 +0200

    Test github actions workflow

--- src/main/java/com/ghrer/ecommerce/products/controllers/ProductsController.java
+++ src/main/java/com/ghrer/ecommerce/products/controllers/ProductsController.java
@@ -1,6 +1,7 @@
 package com.ghrer.ecommerce.products.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import reactor.core.publisher.Mono;
@@ -11,6 +12,11 @@ public class ProductsController {
 	@GetMapping("/")
 	public Mono<String> getProducts(){
 		return Mono.just("Hello");
+	}
+	
+	@GetMapping("/{name}")
+	public Mono<String> salut(@PathVariable String name){
+		return Mono.just("Hello: " + name);
 	}
 
 }
